1de49ccd66a573cb4709d1f5facbfc5a2619bd0d
Schuyler Erle <schuyler@nocat.net>
2025-03-26 09:33:04 -0700

    feat(database): split out FoursquareOSP class

--- garganorn/database.py
+++ garganorn/database.py
@@ -33,52 +33,71 @@ class Database:
             self.conn.close()
             self.conn = None
             
-    def execute(self, query):
+    def execute(self, query, params=None):
         """Execute a query on the database."""
         if not self.conn:
             self.connect()
-        stmt = self.conn.execute(query)
+        stmt = self.conn.execute(query, params)
         rows = stmt.fetchall()
         columns = tuple(c[0] for c in stmt.description)
         return [dict(zip(columns, row)) for row in rows]
 
+    def query_nearest(self):
+        raise NotImplementedError
+    
+    def process_nearest(self, result):
+        return result
+
     def nearest(self, latitude, longitude, expand_m=5000, limit=50):
+        # Expand the bounding box around the point by roughly expand_m meters
         expand_lat = expand_m / DEG_TO_M
         expand_lon = expand_lat / math.cos(latitude * DEG_TO_RAD) if math.fabs(latitude) < 90 else expand_lat
         bbox = (max((longitude - expand_lon, -180)),
                 max((latitude - expand_lat, -90)),
                 min((longitude + expand_lon, 180)),
                 min((latitude + expand_lat, 90)))
-        """Find the nearest location to a given latitude and longitude."""
-        query = """
+        result = self.execute(
+            self.query_nearest(), 
+            {
+                "centroid": f"POINT({longitude} {latitude})",
+                "xmin": bbox[0],
+                "ymin": bbox[1],
+                "xmax": bbox[2],
+                "ymax": bbox[3],
+                "limit": limit
+            }
+        )
+        return [self.process_nearest(item) for item in result]
+
+class FoursquareOSP(Database):
+    def query_nearest(self):
+        return """
             select 
-            concat('foursquare.com/v/', fsq_place_id) as id,
-            name,
-            latitude::decimal(10,6)::varchar as latitude,
-            longitude::decimal(10,6)::varchar as longitude,
-            address,
-            locality,
-            postcode,
-            region,
-            country,
-            date_created,
-            date_refreshed,
-            fsq_category_labels,
-            ST_Distance_Sphere(geom, ST_GeomFromText('POINT({lon} {lat})'))::integer as distance_m,
+                concat('foursquare.com/v/', fsq_place_id) as id,
+                name,
+                latitude::decimal(10,6)::varchar as latitude,
+                longitude::decimal(10,6)::varchar as longitude,
+                address,
+                locality,
+                postcode,
+                region,
+                country,
+                date_created,
+                date_refreshed,
+                fsq_category_labels,
+                ST_Distance_Sphere(geom, ST_GeomFromText($centroid))::integer as distance_m,
             from places
-            where bbox.xmin > {bbox[0]} and bbox.ymin > {bbox[1]} and bbox.xmax < {bbox[2]} and bbox.ymax < {bbox[3]}
-            and date_refreshed > '2020-03-15'
-            and date_closed is null
+            where bbox.xmin > $xmin and bbox.ymin > $ymin and bbox.xmax < $xmax and bbox.ymax < $ymax
+                and date_refreshed > '2020-03-15'
+                and date_closed is null
             order by distance_m
-            limit {limit};
-        """.format(bbox=bbox, lat=latitude, lon=longitude, limit=limit)
-        result = self.execute(query)
-        return result
+            limit $limit;
+            """
 
 
 if __name__ == "__main__":
     from pprint import pprint
-    d = Database("db/fsq-osp.duckdb")
+    d = FoursquareOSP("db/fsq-osp.duckdb")
     result = d.nearest(37.776145, -122.433898)
     pprint(result)
     d.close()
