5456f978bbe5c98381bb27d0a7e47c3a4fde086b
Schuyler Erle <schuyler@nocat.net>
2026-03-30 10:47:57 -0700

    remove obsolete SQL import scripts
    Superseded by import-fsq-extract.sh and import-overture-extract.sh.

--- scripts/import-overture-places.sql
+++
@@ -1,8 +0,0 @@
-install spatial;
-load spatial;
-
-create table places as select * from 's3://overturemaps-us-west-2/release/2025-03-19.1/theme=places/type=place/part-00000*.parquet' limit 0;
-copy places from 's3://overturemaps-us-west-2/release/2025-03-19.1/theme=places/type=place/*.parquet';
-delete from places where geometry is null;
-attach 'overture.duckdb' as output;
-copy from database memory to output;
